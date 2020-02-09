60a095aa71da7d12575e735e9fffa23d98682fad
wenbo <1802161998@qq.com>
2020-02-10 01:13:15 +0800

    新建购物车模块-接收参数

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django import http
 from django.views import View
+from goods.models import SKU
+import json
 # Create your views here.
 
 class CartsView(View):
@@ -7,8 +10,28 @@ class CartsView(View):
     def post(self,request):
         '''保存购物车'''
         # 接收参数
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get('sku_id')
+        count = json_dict.get('count')
+        selected = json_dict.get('selected')
 
         # 校验参数
+        #判断参数是否齐全
+        if not all(sku_id,count):
+            return http.HttpResponseForbidden('缺少必填参数')
+        try:
+            SKU.objects.get(sku_id)
+        except SKU.DoesNotExist:
+            return http.HttpResponseForbidden('参数sku_id错误')
+        # 校验是否收是数字
+        try:
+            count = int(count)
+        except Exception as e:
+            return http.HttpResponseForbidden('参数count错误')
+        # 校验是否被勾选：bool
+        if selected:
+            if isinstance(selected,bool):
+                return http.HttpResponseForbidden('参数selected错误')
 
         # 判断用户是否登陆
 

d477e95c387b8bb8002bd907f3454db6f2ae6e83
wenbo <1802161998@qq.com>
2020-02-10 02:05:26 +0800

    新建购物车模块-登陆状态添加购物车

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django import http
 from django.views import View
+from django_redis import get_redis_connection
+
 from goods.models import SKU
 import json
 # Create your views here.
+from meiduo_mall.utils.response_code import RETCODE
+
 
 class CartsView(View):
     '''购物车管理'''
@@ -13,7 +17,7 @@ class CartsView(View):
         json_dict = json.loads(request.body.decode())
         sku_id = json_dict.get('sku_id')
         count = json_dict.get('count')
-        selected = json_dict.get('selected')
+        selected = json_dict.get('selected',True)
 
         # 校验参数
         #判断参数是否齐全
@@ -30,13 +34,25 @@ class CartsView(View):
             return http.HttpResponseForbidden('参数count错误')
         # 校验是否被勾选：bool
         if selected:
-            if isinstance(selected,bool):
+            if not isinstance(selected,bool):
                 return http.HttpResponseForbidden('参数selected错误')
 
         # 判断用户是否登陆
         user = request.user
         if user.is_authenticated:
             # 如果用户已登陆，操作redis购物车
+            redis_conn = get_redis_connection('carts')
+            pl = redis_conn.pipeline()
+
+            # 需要以增量计算的形式保存商品数量
+            pl.hincrby('carts_%s' % user.id,sku_id,count)
+            # 保存商品勾选状态
+            if selected:
+                pl.sadd('selected_%s' % user.id,sku_id)
+            # 执行
+            pl.execute()
+            # 响应结果
+            return http.JsonResponse({'code':RETCODE.OK,'errmsg':'OK'})
             pass
         else:
             # 如果未登陆，操作cookie购物车

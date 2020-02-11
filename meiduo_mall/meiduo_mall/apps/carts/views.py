283333d5505592232f6f4a6d1dbfbc3cb340c590
wenbo <1802161998@qq.com>
2020-02-11 16:05:34 +0800

    新建购物车模块-删除购物车视图

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -261,3 +261,62 @@ class CartsView(View):
 
             # 响应结果
             return response
+
+    def delete(self, request):
+        """删除购物车"""
+        # 接收参数
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get('sku_id')
+
+        # 判断sku_id是否存在
+        try:
+            SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return http.HttpResponseForbidden('商品不存在')
+
+        # 判断用户是否登录
+        user = request.user
+        if user is not None and user.is_authenticated:
+            # 用户已登录，删除redis购物车
+            redis_conn = get_redis_connection('carts')
+            pl = redis_conn.pipeline()
+
+            # 删除hash购物车商品记录
+            pl.hdel('carts_%s' % user.id, sku_id)
+            # 同步移除勾选状态
+            pl.srem('selected_%s' % user.id, sku_id)
+            pl.execute()
+
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+        else:
+            # 用户未登录，删除cookie购物车
+            # 获取cookie中的购物车数据，并且判断是否有购物车数据
+            cart_str = request.COOKIES.get('carts')
+            if cart_str:
+                # 将 cart_str转成bytes类型的字符串
+                cart_str_bytes = cart_str.encode()
+                # 将cart_str_bytes转成bytes类型的字典
+                cart_dict_bytes = base64.b64decode(cart_str_bytes)
+                # 将cart_dict_bytes转成真正的字典
+                cart_dict = pickle.loads(cart_dict_bytes)
+            else:
+                cart_dict = {}
+
+            # 构造响应对象
+            response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+
+            # 删除字典指定key所对应的记录
+            if sku_id in cart_dict:
+                del cart_dict[sku_id] # 如果删除的key不存在，会抛出异常
+
+                # 将cart_dict转成bytes类型的字典
+                cart_dict_bytes = pickle.dumps(cart_dict)
+                # 将cart_dict_bytes转成bytes类型的字符串
+                cart_str_bytes = base64.b64encode(cart_dict_bytes)
+                # 将cart_str_bytes转成字符串
+                cookie_cart_str = cart_str_bytes.decode()
+
+                # 写入新的cookie
+                response.set_cookie('carts', cookie_cart_str)
+
+            return response
